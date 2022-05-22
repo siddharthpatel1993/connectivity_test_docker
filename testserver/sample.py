864d647f523b0568352ad34ed8a2f8d033f76c23
siddharthpatel1993 <59063709+siddharthpatel1993@users.noreply.github.com>
2022-05-22 10:22:15 -0500

    Update sample.py

--- testserver/sample.py
+++ testserver/sample.py
@@ -2,6 +2,19 @@ import subprocess
 import re
 import time
 
+def countdown():
+        t = 5
+        while t:
+                mins, secs = divmod(t, 60)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                print("*****Waiting Time Countdown for 5 seconds ==> ",timer, "*****")
+                #print(timer,end="\r")
+                time.sleep(1)
+                t -= 1
+
+        print('Times up !!!')
+
+
 def url_service_function():
     x = subprocess.check_output("cat configuration_and_log/test.txt | grep -v '^#' | sed '/^$/d;s/[[:blank:]]//g' > configuration_and_log/test1.txt", shell=True)
 
@@ -17,9 +30,11 @@ def url_service_function():
         host_num = count
 
         a1 = "{}".format(line.strip())
-        print(a1)
+        print("Moving file/dir ", a1)
+        countdown()
 
         subprocess.call("mv /var/log/"+ a1 +" conf/", shell=True)
 
 if __name__=="__main__":
     url_service_function()
+    #countdown()
